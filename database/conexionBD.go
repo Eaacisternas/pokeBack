b7cbe8e657427e11bc6b8d4c5e40e7d883b0f7fc
Eduardo Aguirre <eaacisternass@gmail.com>
2022-08-07 02:20:49 -0400

    Lee api pokemon, y se integra id, nombre, peso

---
+++ database/conexionBD.go
@@ -0,0 +1,38 @@
+package bd
+
+import (
+	"context"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+/*MongoCN es el objeto de conexión a la base de datos*/
+var MongoCN = ConectarBD()
+var clientOptions = options.Client().ApplyURI("mongodb+srv://eduardoAguirre:Abril201118@cluster0.ep7djah.mongodb.net/?retryWrites=true&w=majority")
+
+/*ConerctarBD es la función que me permite conectar la BD*/
+func ConectarBD() *mongo.Client {
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err.Error())
+		return client
+	}
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err.Error())
+		return client
+	}
+	log.Println("Conexión exitosa con la base de datos")
+	return client
+}
+
+/*valConnectiones hace el ping a la Bd*/
+func ValConnection() int {
+	err := MongoCN.Ping(context.TODO(), nil)
+	if err != nil {
+		return 0
+	}
+	return 1
+}
--- main.go
+++ main.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"log"
+
+	"github.com/eaacisternas/pokeBack/handlers"
+)
+
+func main() {
+	if bd.ValConnection() == 0 {
+		log.Fatal("No se pudo conectar a la bd")
+		return
+	}
+	handlers.Manejadores()
+}
